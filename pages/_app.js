fcf47fa762f05aa166b14e7743d50f83f5cb3283
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2022-11-04 22:03:49 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import mongoose from 'mongoose'
 
 function MyApp({ Component, pageProps }) {
 
